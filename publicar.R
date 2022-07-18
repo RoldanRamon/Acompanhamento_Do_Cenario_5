f027cd83aa74d44a759ea2dfcc1255285075bab5
Ramon Roldan de Lara <roldanramon83@gmail.com>
2022-07-18 09:12:01 -0300

    Adicionando linha para limpar enviroment apos rodar script

--- publicar.R
+++ publicar.R
@@ -2,4 +2,5 @@
 library(rsconnect)
 result <- rpubsUpload(title = "Acompanhamento do Cenário 5", contentFile = "18.Julho-Acompanhamento.html",originalDoc = "07.Julho-Acompanhamento.html")
 browseURL(result$continueUrl)
+rm(list = ls())
 
