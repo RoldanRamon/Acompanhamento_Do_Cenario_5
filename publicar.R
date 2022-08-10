b1b7932c212a03647a1e1ceb9193d9671bf97fa2
Ramon Roldan de Lara <roldanramon83@gmail.com>
2022-08-10 09:52:36 -0300

    Rodando atualizacao

--- publicar.R
+++ publicar.R
@@ -1,6 +1,6 @@
 
 library(rsconnect)
-result <- rpubsUpload(title = "Acompanhamento do Cenário 5", contentFile = "4- relatorios/08.Agosto-Acompanhamento.html",originalDoc = "07.Julho-Acompanhamento.html")
+result <- rpubsUpload(title = "Acompanhamento do Cenário 5", contentFile = "4- relatorios/10.Agosto-Acompanhamento.html",originalDoc = "07.Julho-Acompanhamento.html")
 browseURL(result$continueUrl)
 rm(list = ls())
 
